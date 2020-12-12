d6186161f90c01231911ffc3ce57c5453da4f964
Miguel <miguelfervi@gmail.com>
2020-12-12 16:35:03 +0100

    ADD test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,27 @@
-const { it } = require('@jest/globals');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+const { it, expect, beforeEach } = require('@jest/globals');
 
 describe('Appointment', () => {
-  it('renders the customer first name', () => {});
+  let container;
+  let customer;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = (component) => ReactDOM.render(component, container);
+
+  it('renders the customer first name', () => {
+    customer = { firstName: 'Ashley' };
+    render(<Appointment customer={customer}></Appointment>);
+    expect(container.textContent).toMatch('Ashley');
+  });
+
+  it('renders another customer first name', () => {
+    customer = { firstName: 'Jordan' };
+    render(<Appointment customer={customer}></Appointment>);
+    expect(container.textContent).toMatch('Jordan');
+  });
 });
